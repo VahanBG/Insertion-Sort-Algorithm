600ab6d82c7fb5d3cf0e7b43527871d70e32f7bd
VahanBG <vahan365105@gmail.com>
2022-05-20 23:41:32 +0400

    next commit

--- Sort_all_conteners.cpp
+++ Sort_all_conteners.cpp
@@ -10,17 +10,26 @@
 #include<map>
 #include<stack>
 
-void sort(std::vector<int>::iterator it_first,std::vector<int>::iterator it_last) //սա դասավորում է զանգվածի ելեմենտները աճման կարգով
+void sort(std::list<int>::iterator it_first,std::list<int>::iterator it_last) //սա դասավորում է զանգվածի ելեմենտները աճման կարգով
 {
-
-    for(int i = 1 ; i < std::distance(it_first , it_last) ; i++){
-    int count = *( it_first+ i );
-        for(int j = i - 1 ; j >= 0  &&  *(it_first+j) > count ; j--){
-           * (it_first+j+1) = *(it_first+j);
-            *(it_first+j) = count ;
-        }  
-        
+  
+ while( it_first != it_last)
+ {
+     int i = 0 ;
+    while ( it_last != it_first){
+   
+     if(*it_first > *it_last)
+     std::iter_swap(it_first , it_last);
+     it_last -- ;
+     i++;
     }
+     std::advance(it_last , i);
+     it_first++ ;
+     
+
+}
+
+    
 }
 
 
@@ -29,16 +38,17 @@ int main ()
   std::list<int > mylist={ 19,15,8,3,47};
   std::list<int>::iterator it_first = mylist.begin();
   auto it_last = mylist.end();
+  it_last --;
   
   std::cout << "mylist contains:";
   for (auto it=mylist.begin(); it!=mylist.end(); ++it)
     std::cout << ' ' << *it;
-  //std::cout << std::endl;
-  sort(it_first,it_last);
-
+  std::cout << std::endl;
+ sort(it_first,it_last);
+std::cout << "mylist contains:";
  for (it_first=mylist.begin(); it_first!=mylist.end(); ++it_first)
     std::cout << ' ' << *it_first;
-  std::cout << std::endl;
+std::cout << std::endl;
 
   return 0;
 }
