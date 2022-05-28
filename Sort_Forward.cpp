b3c62b8474ef26f25248cd47cfe6f6465d8fce41
VahanBG <vahan365105@gmail.com>
2022-05-28 14:16:22 +0400

    next commit

---
+++ Sort_Forward.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+#include<iterator>
+#include<array>
+#include<vector>
+#include<deque>
+#include<list>
+#include<forward_list>
+#include<set>
+#include<map>
+#include<stack>
+
+void sort(std::forward_list<int> arr ,std::forward_list<int>::iterator it_ferst) //սա դասավորում է զանգվածի ելեմենտները աճման կարգով
+{
+    while (it_ferst!= arr.end()){
+    auto it_count = it_ferst;
+    it_ferst ++;
+        if(*it_count > it_ferst){
+        std::iter_swap( it_count , it_ferst);
+        }
+        
+    } 
+}
+
+int main ()
+{
+  std::forward_list<int > my_flist={ 19,15,8,3,47};
+  std::forward_list<int>::iterator it_first = my_flist.begin();
+  std::cout<<*it_first<<std::endl;
+    sort(my_flist, it_first);
+
+
+  /*auto it_last = mylist.end();
+  it_last --;
+  
+  std::cout << "mylist contains:";
+  for (auto it=mylist.begin(); it!=mylist.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << std::endl;
+ sort(it_first,it_last);
+std::cout << "mylist contains:";
+ for (it_first=mylist.begin(); it_first!=mylist.end(); ++it_first)
+    std::cout << ' ' << *it_first;
+std::cout << std::endl;*/
+
+  return 0;
+}
--- Sort_vector.cpp
+++ Sort_vector.cpp
@@ -8,8 +8,8 @@
 #include<set>
 #include<map>
 #include<stack>
-
-void sort(std::vector<int>::iterator arr,int size_arr) //սա դասավորում է զանգվածի ելեմենտները աճման կարգով
+ //template< typename T1 ,typename T2 > 
+void sort(std::vector< int >::iterator arr , size_t size_arr) //սա դասավորում է զանգվածի ելեմենտները աճման կարգով
 {
     for(int i = 1 ; i < size_arr  ; i++){
     int count = *(arr+i);
@@ -20,8 +20,9 @@ void sort(std::vector<int>::iterator arr,int size_arr) //սա դասավորու
         
     }
 }
-void print_arr(std::vector<int>::iterator  arr,int size_arr){ //տպում է զանգվածը
-  for(int i = 0 ; i < size_arr; i++ ){
+
+void print_arr(std::vector< int >::iterator  arr,size_t size_arr){ //տպում է զանգվածը
+  for( int i = 0 ; i < size_arr; i++ ){
         std::cout<<*(arr + i)<<" ";
     }
     std::cout<<std::endl;
@@ -29,23 +30,27 @@ void print_arr(std::vector<int>::iterator  arr,int size_arr){ //տպում է զ
 
 int main()
 {
-     int size_of_array ;
+   
+     /*int size_of_array ;
      std::cout<<"please enter size of numbers in your array"<<std::endl;
      std::cin >> size_of_array ;
      std::cout<<"please enter numbers of your array"<<std::endl;
-     std::vector <int> myVector;
+     std::vector < int > myVector;
      int numbers ;
-     for(int i = 0 ; i < size_of_array ; i++){
+     for( int i = 0 ; i < size_of_array ; i++){
          std::cout<<"enter value of "<< i << " index " <<std::endl ;
          std::cin>> numbers;
          myVector.push_back(numbers);
          numbers = 0 ;
-     }
-     std::vector<int>::iterator my_iterator = myVector.begin();
+     }*/
+int size = 8 ;
+std::vector < int > myVector={ 55 , 88, 44,77,99,66,11,22};
+      
+     std::vector< int >::iterator my_iterator = myVector.begin();
      std::cout<<"your array is : " << std ::endl ;
-     print_arr( my_iterator , size_of_array);
-     sort( my_iterator,size_of_array);
+     print_arr( my_iterator , myVector.size());
+    sort( my_iterator,myVector.size());
      std::cout<<"your array after sorting" << std::endl;
-     print_arr( my_iterator , size_of_array );
+     print_arr( my_iterator , myVector.size() );
    
 }
