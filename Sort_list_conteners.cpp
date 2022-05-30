72d8c63b69e49f4a775af2c624fd063d624fa57e
VahanBG <vahan365105@gmail.com>
2022-05-30 17:31:01 +0400

    next commit

--- Sort_list_conteners.cpp
+++ Sort_list_conteners.cpp
@@ -1,49 +1,42 @@
-#include<iostream>
-#include<string>
-#include<list>
-#include<iterator>
+#include <iostream>
+#include <string>
+#include <list>
+#include <iterator>
 
-
-
-void sort(std::list<int>::iterator it_first,std::list<int>::iterator it_last) // this sorts the elements of the array in ascending order
+void sort(std::list<int>::iterator it_first, std::list<int>::iterator it_last)	// this sorts the elements of the array in ascending order
 {
-  
- while( it_first != it_last)
- {
-     int i = 0 ;
-    while ( it_last != it_first){
-   
-     if(*it_first > *it_last)
-     std::iter_swap(it_first , it_last);
-     it_last -- ;
-     i++;
-    }
-     std::advance(it_last , i);
-     it_first++ ;
-     
 
+	while (it_first != it_last)
+	{
+		int i = 0;
+		while (it_last != it_first)
+		{
+
+			if (*it_first > *it_last)
+				std::iter_swap(it_first, it_last);
+			it_last--;
+			i++;
+		}
+		std::advance(it_last, i);
+		it_first++;
+	}
 }
 
-    
-}
-
-
-int main ()
+int main()
 {
-  std::list<int > mylist={ 19,15,8,3,47};
-  std::list<int>::iterator it_first = mylist.begin();
-  auto it_last = mylist.end();
-  it_last --;
-  
-  std::cout << "mylist contains:";
-  for (auto it=mylist.begin(); it!=mylist.end(); ++it)
-    std::cout << ' ' << *it;
-  std::cout << std::endl;
- sort(it_first,it_last);
-std::cout << "mylist contains:";
- for (auto it_first=mylist.begin(); it_first!=mylist.end(); ++it_first)
-    std::cout << ' ' << *it_first;
-std::cout << std::endl;
+	std::list<int> mylist = { 19, 15, 8, 3, 47 };
+	std::list<int>::iterator it_first = mylist.begin();
+	auto it_last = mylist.end();
+	it_last--;
+	std::cout << "mylist contains:";
+	for (auto it = mylist.begin(); it != mylist.end(); ++it)
+		std::cout << ' ' << *it;
+	std::cout << std::endl;
+	sort(it_first, it_last);
+	std::cout << "mylist contains:";
+	for (auto it_first = mylist.begin(); it_first != mylist.end(); ++it_first)
+		std::cout << ' ' << *it_first;
+	std::cout << std::endl;
 
-  return 0;
+	return 0;
 }
--- Sort_vector.cpp
+++ Sort_vector.cpp
@@ -1,50 +1,52 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<iterator>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
 
- void sort(std::vector< int > arr ) // this sorts the elements of the array in ascending order
+void sort(std::vector<int> arr)	// this sorts the elements of the array in ascending order
 {
-    std::vector< int >::iterator it_vec = arr.begin();
-    for(int i = 1 ; i < arr.size()  ; i++){
-      int count = *(it_vec+i);
-        for(int j = i - 1 ; j >= 0  &&  *(it_vec+j) > count ; j--){
-           * (it_vec+j+1) = *(it_vec+j);
-            *(it_vec+j) = count ;
-        }  
-        
-    }
+	std::vector<int>::iterator it_vec = arr.begin();
+	for (int i = 1; i < arr.size(); i++)
+	{
+		int count = *(it_vec + i);
+		for (int j = i - 1; j >= 0 && *(it_vec + j) > count; j--)
+		{ *(it_vec + j + 1) = *(it_vec + j);
+			*(it_vec + j) = count;
+		}
+	}
 }
 
-void print_arr(std::vector< int >  arr){ 
-  std::vector< int >::iterator my_iterator = arr.begin();
-  for( int i = 0 ; i < arr.size(); i++ ){
-        std::cout<<*(my_iterator + i)<<" ";
-    }
-    std::cout<<std::endl;
+void print_arr(std::vector<int> arr)
+{
+	std::vector<int>::iterator my_iterator = arr.begin();
+	for (int i = 0; i < arr.size(); i++)
+	{
+		std::cout << *(my_iterator + i) << " ";
+	}
+	std::cout << std::endl;
 }
 
 int main()
 {
-   
-     int size_of_array ;
-     std::cout<<"please enter size of numbers in your array"<<std::endl;
-     std::cin >> size_of_array ;
-     std::cout<<"please enter numbers of your array"<<std::endl;
-     std::vector < int > myVector;
-     int numbers ;
-     for( int i = 0 ; i < size_of_array ; i++){
-         std::cout<<"enter value of "<< i << " index " <<std::endl ;
-         std::cin>> numbers;
-         myVector.push_back(numbers);
-         numbers = 0 ;
-     }
 
-      
-     std::cout<<"your array is : " << std ::endl ;
-     print_arr( myVector );
-     sort( myVector);
-     std::cout<<"your array after sorting" << std::endl;
-     print_arr( myVector );
-   
+	int size_of_array;
+	std::cout << "please enter size of numbers in your array" << std::endl;
+	std::cin >> size_of_array;
+	std::cout << "please enter numbers of your array" << std::endl;
+	std::vector<int> myVector;
+	int numbers;
+	for (int i = 0; i < size_of_array; i++)
+	{
+		std::cout << "enter value of " << i << " index " << std::endl;
+		std::cin >> numbers;
+		myVector.push_back(numbers);
+		numbers = 0;
+	}
+
+	std::cout << "your array is : " << std::endl;
+	print_arr(myVector);
+	sort(myVector);
+	std::cout << "your array after sorting" << std::endl;
+	print_arr(myVector);
+
 }
