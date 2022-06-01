3d4496e847b8ec481a67051fdc4e6064a8420dbd
VahanBG <vahan365105@gmail.com>
2022-06-01 20:28:30 +0400

    next commit

--- Sort_whith_iterator.cpp
+++ Sort_whith_iterator.cpp
@@ -2,16 +2,14 @@
 #include <list>
 #include <vector>
 #include <iterator>
-template < typename T >
-void sort( T it_first , T it_last )// this sorts the elements of the array in ascending order
-{
-   
+template < typename T1  >
+void sort(T1  it_first , T1  it_last)// this sorts the elements of the array in ascending order
+{   
 	while (it_first != it_last)
 	{
 		int i = 0;
 		while (it_last != it_first)
 		{
-
 			if (*it_first > *it_last)
 				std::iter_swap(it_first, it_last);
 			it_last--;
@@ -22,7 +20,7 @@ void sort( T it_first , T it_last )// this sorts the elements of the array in as
 	}
 }
 template < typename T>
-void Print( T my_contener)
+void Print(T my_contener)
 {
     for (auto it_first = my_contener.begin(); it_first != my_contener.end(); ++it_first)
 	std::cout << ' ' << *it_first;
@@ -40,12 +38,10 @@ int main()
 	std::cout << "mylist contains:";
 	Print(mylist);
     std::vector<int> myVector = { 19, 15, 8, 3, 47 };
-    std::vector< int >::iterator it_vec_first = myVector.begin();
-    auto it_vec_last = myVector.end() -1;    
+    auto it_vec_first = myVector.begin();
+    auto it_vec_last = myVector.end()-1;
     sort(it_vec_first , it_vec_last);
     std::cout << "myvector contains:";
     Print(myVector);
-
-	
 	return 0;
 }
